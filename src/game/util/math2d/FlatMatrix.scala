cd3c00f5e94b14e76ece7669c336147b4ce131a8
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-09 19:47:47 +0100

    FlatMatrix is now serializable

--- src/game/util/math2d/FlatMatrix.scala
+++ src/game/util/math2d/FlatMatrix.scala
@@ -1,6 +1,10 @@
 package game.util.math2d
 
-class FlatMatrix[T: Manifest](val width: Int, val height: Int, passedData: Option[Array[T]] = None) {
+import scala.reflect.ClassTag
+
+class FlatMatrix[T: ClassTag](val width: Int, val height: Int, passedData: Option[Array[T]] = None) extends Serializable {
+  def uuid = java.util.UUID.randomUUID.toString
+
   val data: Array[T] = passedData.getOrElse(new Array[T](width*height))
 
   def translateIndex(x: Int, y: Int): Int = x*height+y
